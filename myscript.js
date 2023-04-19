fdfb6f84f127bc0d77115bcb2a75025ae4ae2cec
AlessioAbbati <alex_117@live.it>
2023-04-19 15:27:09 +0200

    add list

---
+++ myscript.js
@@ -0,0 +1,18 @@
+/*
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano)
+gli elementi della lista individualmente con un ciclo for.
+*/
+
+const arrList = [
+	'pomodori',
+	'funghi',
+	'latte',
+	'carne',
+	'caffè',
+	'peperoni',
+	'biscotti',
+	'salmone',
+	'pasta',
+];
+
+const eleList = document.querySelector('#list');
