46b8f7069c698dff3f0c5edb273bc587aef967ca
AlessioAbbati <alex_117@live.it>
2023-04-19 17:21:36 +0200

    add while cicle

--- myscript.js
+++ myscript.js
@@ -16,3 +16,18 @@ const arrList = [
 ];
 
 const eleList = document.querySelector('#list');
+
+// soluzione con il ciclo for
+// for (let i = 0; i < arrList.length; i++) {
+// 	console.log(arrList[i]);
+// 	eleList.innerHTML += `<li>${arrList[i]}</li>`;
+// }
+
+// soluzione con il ciclo while
+
+let i = 0
+while (i < arrList.length) {
+    console.log(arrList[i]);
+    i++;
+    eleList.innerHTML += `<li>${arrList[i]}</li>`;
+}
